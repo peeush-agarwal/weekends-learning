26ac9bc0c10e7e12a313f1dc4bf8b31c8381d9ec
Peeush Agarwal <agarwal.peeush@gmail.com>
2020-04-23 21:30:40 +0530

    Club several images into 1 image of specific size

---
+++ Deep-Learning/OpenCv/PyImageSearch-tutorials/build_montages.py
@@ -0,0 +1,21 @@
+import argparse
+import cv2
+from imutils import build_montages
+from imutils import paths
+import random
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--images", required=True, help="Path to input images")
+ap.add_argument("-n", "--num-of-images", type=int, default=21, help="number of images to be montaged together")
+args = vars(ap.parse_args())
+
+image_paths = paths.list_images(args["images"])
+
+images = []
+for img_path in image_paths:
+    images.append(cv2.imread(img_path))
+
+montages = build_montages(images, (128, 196), (7,3))
+for montage in montages:
+    cv2.imshow("Montage", montage)
+    cv2.waitKey(0)
